073fefdd1a690dfb8f79e5d86baf19e56155f951
davide-unipv <62263184+davide-unipv@users.noreply.github.com>
2021-02-10 15:18:51 +0100

    sistemato iterazioni successive

--- code/inverse.cpp
+++ code/inverse.cpp
@@ -22,24 +22,21 @@ void showMatrix(float **matrix, int size){
 }
 
 void create_Matrix (float **random, int size){
-    
     int i, j;
     int range = MAXNUMBER - MINNUMBER;
-    //srand(static_cast<unsigned>(time(0))+number); 
     srand(time(NULL));
-    //srand(0);
         for(i = 0; i <size; i++)
             for(j = 0; j< size; j++)
                 random[i][j] = rand() %(range);
 }
 
 void lu(float **a, float **l, float **u, int size){
-    int i = 0, j = 0, k = 0;
+    //int i = 0, j = 0, k = 0; //danno problemi queste variabili condivise??
     #pragma omp parallel for
-    for (k = 0; k < size; k++){
-        for (i = k+1; i < size; i++){
+    for (int k = 0; k < size; k++){
+        for (int i = k+1; i < size; i++){
                 l[i][k] = u[i][k] / u[k][k];
-                for (j = k; j < size; j++){
+                for (int j = k; j < size; j++){
                     u[i][j] = u[i][j] - l[i][k] * u[k][j];
                 }
             }
@@ -47,9 +44,10 @@ void lu(float **a, float **l, float **u, int size){
 }
 
 void pivoting(float **a, float **p, int size){
-    bool flag=false;
+    bool flag=false; //possibile problema di variabile condivisa
     #pragma omp parallel for // le iterazioni sono indipendenti tra di loro, quindi posso parallelizzare
-    for (int k = 0; k < size-1; k++){   
+    
+	 for (int k = 0; k < size-1; k++){   
         int imax = 0;
         //foreach column i need to find which row has the maximum (in module) value
         for (int j = k; j < size; j++){
@@ -72,9 +70,9 @@ void pivoting(float **a, float **p, int size){
 }
 
 void forwardSubst(float **l, float **p,int column, float *y, int size){
-  	cout<< "\nForward colonna:"<<column<<"\n";
+  	//cout<< "\nForward colonna:"<<column<<"\n";
 	//y[0] = p[0][column] / l[0][0];
-	cout<< "\n00 :"<<y[0]<<"\n";
+	//cout<< "\n00 :"<<y[0]<<"\n";
 	float sum=0;
     for(int i = 0; i< size; i++){
     	
@@ -114,10 +112,6 @@ void backwardSubst(float **u, float *y, float **a1, int column, int size){
 }
 
 void findInverse(float **a, float **a1, float **l, float **u, float **p, int size){
-    /**
-     * 
-     */
-
     //i can do each column indipendentily
     #pragma omp parallel for 
     for(int i=0; i< size; i++){
@@ -132,7 +126,7 @@ void findInverse(float **a, float **a1, float **l, float **u, float **p, int siz
 void multiply(float **a, float **b, float **r, int size){
     //the program will work only on square matrices
     //multiply a*b
-    #pragma omp parallel for collapse(2) // because 3 has some dependances that will create a wrong result
+    #pragma omp parallel for collapse(3) 
         for(int i = 0; i < size; i++)
             for(int j = 0; j < size; j++)
                 for(int k = 0; k < size; k++)
@@ -150,8 +144,6 @@ double execution (int size,int threadcount){
     float **p = (float **)malloc(size * sizeof(float*));
     float **r = (float **)malloc(size * sizeof(float*));
     float **a1 = (float **)malloc(size * sizeof(float*));
-    
-    
     float **a_p = (float **)malloc(size * sizeof(float*));
     
     #pragma omp parallel for 
@@ -181,12 +173,14 @@ double execution (int size,int threadcount){
     
     
     create_Matrix(a,size);
-    cout << "\nMatrix A:\n";
-    showMatrix(a, size);
+    //cout << "\nMatrix A:\n";
+    //showMatrix(a, size);
     //cout << "\nPivoting matrix:\n";
     //showMatrix(p,size);
-    double time= omp_get_wtime();
     
+	
+	
+	double time= omp_get_wtime();
     #pragma omp parallel for   
     for (i = 0; i < size; i++) {
         l[i][i] = 1;
@@ -197,56 +191,42 @@ double execution (int size,int threadcount){
                 l[i][j] = 0;
         }
     }
-    
     /**
      * P*a = l*u and LUx =p
      * where p is the column to pivot of the b matrix and x is a column of the inverse
     */
-    cout << "\nPivoting....\n";
+    //cout << "\nPivoting....\n";
     pivoting(a_p,p,size);
-    
     #pragma omp parallel for
     for (i = 0; i < size; i++)
         for (j = 0; j < size; j++)
             u[i][j] = a_p[i][j];
-
-    cout << "\nMatrix A pivottata:\n";
-    showMatrix(a_p, size);
+    //cout << "\nMatrix A pivottata:\n";
+    //showMatrix(a_p, size);
     //cout << "\nPivoting matrix:\n";
     //showMatrix(p, size);
-    
     lu(a_p,l,u,size);
-     
     /* TEST LU
-    */ 
     cout << "\nL matrix:\n";
     showMatrix(l, size);
     cout << "\nU matrix:\n";
     showMatrix(u, size);
-
     cout << "\nL*U=R";
     multiply(l,u,r, size);
     showMatrix(r, size);
+    */
     
-    /*
-    for (i = 0; i < size; i++)
-        for (j = 0; j < size; j++)
-            r[i][j] = 0;
-   */
-
-    
-
    /**
     * solving the system LUxi=Pi where i is the column and x is the column
     * of the inverse to find
     */
-    cout << "\nFinding the inverse...";
+    //cout << "\nFinding the inverse...";
     findInverse(a_p,a1,l,u,p,size);
     
-    cout<< "\nMatrix a^-1:\n";
-    showMatrix(a1, size);
+    //cout<< "\nMatrix a^-1:\n";
+    //showMatrix(a1, size);
 
-    //time = omp_get_wtime()-time;
+    time = omp_get_wtime()-time;
     //cout << "\nExecution time: "<< time << "\n\n";
     
     //multiply(a,a1,r);
@@ -262,32 +242,28 @@ double execution (int size,int threadcount){
 
 int main(int argc,char **argv){
         
-/*
-    int dimension[]={2000,4000,6000,8000,10000,20000};
-    int threadcount[]={1,2,4,6,8,12,24};
+
+    int dimension[] = { 500,1000,1500,2000,2500,3000 };
+	int threadcount[] = { 5,6,8 };
     double avgtime;
-	ofstream outfile;
-	outfile.open("Test_results_inverse.txt");
-    //int size = atoi(argv[1]);
-    for (int i = 0; i < 7; i++)
-    {
-        cout<<"\n\nThreads: "<<threadcount[i]<<"\nSize:\tTime AVG:\n";
-		outfile <<"\n\nThreads: " << threadcount[i] << "\nSize:\tTime AVG:\n";
-        for (int j = 0; j < 6; j++)
-        {
-            avgtime = 0; //reinitilize it
-            cout<<dimension[j]<<"\t";
-			outfile << dimension[j] << "\t";
-            //for (int k =1; k <= 5; k++){
-				avgtime = execution(dimension[j], threadcount[i]);//avgtime + execution(dimension[j],threadcount[i]);
-            //}
-            //avgtime = avgtime/5.0F;
-            cout<<avgtime<<"\n";
-			outfile << avgtime << "\n";
-        }
-    }
-	outfile.close();
-	*/
-	execution(3, 1);
+	//ofstream outfile;
+	//outfile.open("Test_results_inverse.txt");
+    for (int i = 0; i < sizeof(threadcount)/sizeof(threadcount[0]); i++)
+	{
+		cout <<"\n\nNumber of threads: "<< threadcount[i]<<"\n";
+		//outfile <<"\n\nNumber of threads: "<< threadcount[i]<<"\n";
+		for (int j = 0; j < sizeof(dimension)/sizeof(dimension[0]); j++)
+		{
+			avgtime = 0; 
+			cout <<"\nDimension: "<< dimension[j];
+			//outfile <<"\nDimension: "<< dimension[j];
+			avgtime = execution(dimension[j], threadcount[i]); 
+			cout<<"\nTime: "<<avgtime<<"\n";
+			//outfile<<"\nTime: "<<avgtime<<"\n";
+		}
+	}
+	//outfile.close();
+
+	
     return 0;
 }
